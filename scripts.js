475ad9eca35bbc91fb9f5a0665785887ce435d65
Harry L-B <harryloefflerbell@gmail.com>
2023-09-07 09:17:31 -0400

    add basic operation functions

---
+++ scripts.js
@@ -0,0 +1,19 @@
+let number1 = 0;
+let number2 = 0;
+let operator = "";
+
+function add(number1, number2){
+    return number1 + number2;
+}
+
+function subtract(number1,number2){
+    return number1-number2;
+}
+
+function multiply(number1,number2){
+    return number1 * number2;
+}
+
+function divide(number1,number2){
+    return number1/number2;
+}
